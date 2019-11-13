d83df4e162843b84fa814ae7ecb14f414809cce1
@xanderlav <alejandroregino.hussmann.com>
2019-11-12 18:19:27 -0600

    Concert catch error adjustment

--- liri.js
+++ liri.js
@@ -50,14 +50,13 @@ inquirer
 // Execution function in order to get info desired
 function concert_this(arg){
     qryURL = "https://rest.bandsintown.com/artists/" + arg + "/events?app_id=codingbootcamp";
-        axios.get(qryURL).then( function(response) {
-            // console.log(response);
+        axios.get(qryURL).then(function(response) {
             console.log(`<<<<<<< * Results for your search ${arg} * >>>>>>>` + "\n");
             console.log(` Name: ${response.data[0].venue.name} \r\n ` );
             console.log(` Location: ${response.data[0].venue.city} \r\n `);
             console.log(` Date: ` + moment(response.data[0].datetime).format("DD/MM/YYYY") + "\r\n");
     }).catch((error) => {
-        console.log(error, 'Promise error');
+        console.log(error.message, 'Promise error o_O');
     })
 };
 
